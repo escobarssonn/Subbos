a3d63552e03cf3b47bd49d7f4620cc796e9ca8f9
escobarssonn <ahmadridzki03@gmail.com>
2025-01-06 22:15:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ data:[
 features:{
 id:'features',
 css:'col-1-3',
-data:[{id:1,img:1,nama:'BASIC',gambar:'img/figure-01.png',icon:'camera'},
+data:[{id:1,img:1,nama:'BASIC',gambar:'img/figure-01.png',icon:'camera'},https://www.youtube.com/watch?v=OAxXqn0R1zA
 {id:1,img:0,nama:'PRO',gambar:'img/figure-02.png',icon:'whatsapp'},
 {id:1,img:1,nama:'PERFORMANCE',gambar:'img/figure-05.png',icon:'facebook'},
 ]
